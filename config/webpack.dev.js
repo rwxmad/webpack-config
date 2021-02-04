9cedfedf7022201d45d2b13d47156d323e389c6c
madnesstony <themadnesstony@gmail.com>
2021-02-04 20:08:09 +0300

    Remove modules option from css-loader

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -21,7 +21,7 @@ module.exports = merge(common, {
         test: /\.(scss|css)$/,
         use: [
           'style-loader',
-          { loader: 'css-loader', options: { sourceMap: true, importLoaders: 1, modules: true } },
+          { loader: 'css-loader', options: { sourceMap: true, importLoaders: 1 } },
           { loader: 'postcss-loader', options: { sourceMap: true } },
           { loader: 'sass-loader', options: { sourceMap: true } },
         ],
